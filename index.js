af2763854f93e09a8e1e9ebebfe154dc7756832a
Amirize Wisdom <97132123+chukwumela909@users.noreply.github.com>
2025-01-27 12:45:41 +0100

    mnmn

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 1200
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+
+
+
+// const uri = 'mongodb+srv://amirizew:dodo1111@cluster0.nib2hkr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const uri = 'mongodb+srv://amirizew:661SkONRLFmb0cdM@cluster0.fhpv0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+//database  connection
+mongoose.connect(uri,).then(
+    () => {console.log('connected successfully')}
+  );
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+//Routes
+const packageRoute = require("./routes/thePackageRoute")
+// const authenticationRoute = require("./routes/authroute")
+// const dashboardRoute = require("./routes/dashboard")
+
+
+
+//MIDDLEWARES
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    // req.header("Content-Type: application/x-www-form-urlencoded");
+    res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Methods", " GET, POST, OPTIONS, PUT, PATCH, DELETE")
+    // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token");
+
+    res.header(
+        'Access-Control-Expose-Headers',
+        'x-access-token, x-refresh-token'
+    );
+
+    next();
+})
+
+app.get('/', (req, res) => {
+    res.json({message: "I work"});
+});
+
+
+app.use('/package', packageRoute)
+// app.use('/auth', authenticationRoute)
+// app.use('/', dashboardRoute)
+// app.use('/', dashboardRoute)
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
---
+++ routes/thePackageRoute.js
@@ -0,0 +1,116 @@
+const express = require("express");
+const router = express.Router();
+const Package = require("../models/package");
+
+// Route to create a package
+router.post("/create", async (req, res) => {
+  try {
+    const {
+      trackingId,
+      senderName,
+      senderEmail,
+      senderPhone,
+      senderLocation,
+      receiverName,
+      receiverEmail,
+      receiverPhone,
+      receiverLocation,
+      deliveryMode,
+      contentName,
+      contentWeight,
+      deliveryStatus
+    } = req.body;
+
+    // Check if a package with the same trackingId already exists
+    const existingPackage = await Package.findOne({ trackingId });
+    if (existingPackage) {
+      return res.status(400).json({error: true, message: "Package with this tracking ID already exists." });
+    }
+
+    // Create a new package instance
+    const newPackage = new Package({
+      trackingId,
+      senderName,
+      senderEmail,
+      senderPhone,
+      senderLocation,
+      receiverName,
+      receiverEmail,
+      receiverPhone,
+      receiverLocation,
+      deliveryMode,
+      contentName,
+      contentWeight,
+      deliveryStatus
+    });
+
+    // Save the package to the database
+    const savedPackage = await newPackage.save();
+
+    // Respond with the saved package details
+    res.status(201).json(savedPackage);
+  } catch (error) {
+    // Respond with error details
+    res.status(500).json({ message: error.message || "An error occurred while creating the package." });
+  }
+});
+
+router.post("/update/:trackingId", async (req, res, next) => {
+  const { trackingId } = req.params;
+  const {
+   deliveryStatus
+  } = req.body;
+
+
+  const updateData = req.body;
+
+  try {
+    const package = await Package.findOneAndUpdate({ trackingId }, updateData, { new: true });
+
+    if (!package) {
+      return res.status(400).json({ message: 'Package not found' });
+    }
+
+    res.json(package);
+  } catch (error) {
+    console.error('Error updating package:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+router.get("/single/:trackingId", async (req, res, next) => {
+  const { trackingId } = req.params;
+
+  try {
+    const package = await Package.findOne({ trackingId });
+
+    if (!package) {
+      return res.status(400).json({ message: 'Package not found' });
+    }
+
+    res.json(package);
+  } catch (error) {
+    console.error('Error fetching package:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+
+});
+
+router.get("/packages", async (req, res, next) => {
+
+  try {
+    const packages = await Package.find();
+
+    if (!packages) {
+      return res.status(400).json({ message: 'No packages' });
+    }
+
+    res.json(packages);
+  } catch (error) {
+    console.error('Error fetching package:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+
+});
+
+module.exports = router;
