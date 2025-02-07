eff011e0d58115cd72db1dbe5e8920f87d4ec0a8
Amirize Wisdom <97132123+chukwumela909@users.noreply.github.com>
2025-02-07 10:16:27 +0100

    ghgh

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const port = process.env.PORT || 1200
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const Admin = require("./models/admin")
 
 
 
@@ -45,12 +46,80 @@ app.get('/', (req, res) => {
     res.json({message: "I work"});
 });
 
+app.post('/admin-change-password', async (req, res) => {
+    const { username,  newPassword } = req.body;
+
+    if (!username || !newPassword) {
+        return res.status(400).json({ message: 'Please provide all required fields' });
+    }
+
+    try {
+        const admin = await Admin.findOne({ username });
+        if (!admin) {
+            return res.status(404).json({ message: 'Admin not found' });
+        }
+
+        admin.password = newPassword;
+        await admin.save();
+
+        res.status(200).json({ message: 'Password changed successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+});
+
+
+app.post('/create-admin', async (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({ message: 'Please provide all required fields' });
+    }
+
+    try {
+        const existingAdmin = await Admin.findOne({ username });
+        if (existingAdmin) {
+            return res.status(400).json({ message: 'Admin already exists' });
+        }
+
+        const newAdmin = new Admin({ username, password });
+        await newAdmin.save();
+
+        res.status(201).json({ message: 'Admin created successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+});
+
+app.post('/admin-login', async (req, res) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        return res.status(400).json({ message: 'Please provide all required fields' });
+    }
+
+    try {
+        const admin = await Admin.findOne({ username });
+        console.log(admin);
+        if (!admin) {
+            return res.status(400).json({ message: 'Admin not found' });
+        }
+
+        const isMatch = admin.password === password;
+        console.log(isMatch);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Password is incorrect' });
+        }
+
+        res.status(200).json({ message: 'Login successful', admin });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error', error });
+    }
+});
 
 app.use('/package', packageRoute)
-// app.use('/auth', authenticationRoute)
-// app.use('/', dashboardRoute)
-// app.use('/', dashboardRoute)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
